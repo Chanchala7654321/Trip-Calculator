09fb8f15300b60dd38f9a263788c7b4c5890be99
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-03-31 22:07:41 +0530

    Updated the UI

--- Trip Calculator.js
+++ Trip Calculator.js
@@ -5,14 +5,14 @@
 let number1 = document.getElementById("number1");
 let number2 = document.getElementById("number2");
 let cheakBtn = document.getElementById("cheak");
-let total = document.getElementById("total");
+let totalValue = document.querySelector(".total-value");
 
 function calculator() {
     let num1value = number1.value;
     let num2value = number2.value;
 
     let sum = Number(num1value) + Number(num2value);
-    total.innerHTML = `<b>Total:</b> ${sum}`;
+    totalValue.innerHTML = `$${sum.toFixed(2)}`;
 
     number1.value = "";
     number2.value = "";
